9a4b717c6188690a5b8e7f1f839780393ba4bce1
sneha <sneha05162001@gmail.com>
2025-10-29 10:56:22 -0700

    modified

--- app/components/Design.js
+++ app/components/Design.js
@@ -10,7 +10,7 @@ export default function Design() {
 
           </div>
           <div className='col-md-6'>
-            <p className='design-title1'>How to design your site footer like we did</p>
+            <p className='design-title1'>How to design your site footer</p>
             <p className='design-title2'>Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.</p>
             <button className='design-btn'>Learn More</button>
           </div>
